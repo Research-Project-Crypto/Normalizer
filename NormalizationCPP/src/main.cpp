21d8f6649da9fce3059753fab822f84f78528402
Joren-vanGoethem <55790052+Joren-vanGoethem@users.noreply.github.com>
2021-12-22 12:26:49 +0100

    (feat): add percentage difference calculation

--- NormalizationCPP/src/main.cpp
+++ NormalizationCPP/src/main.cpp
@@ -5,13 +5,19 @@ using namespace program;
 struct candle
 {
     std::string event_time;
-    float open;
-    float close;
-    float high;
-    float low;
-    float volume;
+    double open;
+    double close;
+    double high;
+    double low;
+    double volume;
 
-    candle difference(candle& other)
+    double differencelowhigh;
+    double differenceopenclose;
+
+    double maxprofitclose;
+    double maxprofitlowhigh;
+
+    candle difference(candle& other, candle& processedother, bool firstcandle)
     {
         candle candle;
 
@@ -23,6 +29,17 @@ struct candle
         candle.low = (this->low == 0) ? 0 : (other.low - this->low) / this->low;
         candle.volume = (this->volume == 0) ? 0 : (other.volume - this->volume) / this->volume;
 
+        candle.differencelowhigh = (other.low == 0) ? 0 : (other.high - other.low) / other.low;
+        candle.differenceopenclose = (other.open == 0) ? 0 : (other.close - other.open) / other.open;
+
+        if (firstcandle){
+            candle.maxprofitclose = 0;
+            candle.maxprofitlowhigh = 0;
+        } else {
+            candle.maxprofitclose += ((processedother.maxprofitclose) ? 0 : processedother.maxprofitclose) + ((candle.close > 0) ? candle.close : 0);
+            candle.maxprofitlowhigh += ((processedother.maxprofitlowhigh) ? 0 : processedother.maxprofitlowhigh) + ((candle.differencelowhigh > 0) ? candle.differencelowhigh : 0);
+        }
+
         return candle;
     }
 };
@@ -33,10 +50,12 @@ void process_csv(const char* in_file, const char* out)
     io::CSVReader<6> in(in_file);
     
     in.read_header(io::ignore_missing_column | io::ignore_extra_column, "event_time", "open", "close", "high", "low", "volume");
-    csv.newRow() << "event_time" << "open" << "close" << "high" << "low" << "volume";
+    csv.newRow() << "event_time" << "open" << "close" << "high" << "low" << "volume" << "differencelowhigh" << "differenceopenclose" << "maxprofitclose" << "maxprofitlowhigh";
 
     std::stack<candle> candles;
     std::stack<candle> processed_candles;
+
+    bool firstcandle = true;
 
     candle first_candle;
     if (!in.read_row(first_candle.event_time, first_candle.open, first_candle.close, first_candle.high, first_candle.low, first_candle.volume)) return;
@@ -48,10 +67,14 @@ void process_csv(const char* in_file, const char* out)
         candle previous = candles.top();
         candles.pop();
 
-        candle change_candle = previous.difference(next_candle);
+        candle previousProcessed = (processed_candles.empty() ? previous : processed_candles.top());
+        // processed_candles.pop();
+
+        candle change_candle = previous.difference(next_candle, previousProcessed, firstcandle);
         processed_candles.push(change_candle);
+        firstcandle = false;
         
-        csv.newRow() << change_candle.event_time << change_candle.open << change_candle.close << change_candle.high << change_candle.low << change_candle.volume;
+        csv.newRow() << change_candle.event_time << change_candle.open << change_candle.close << change_candle.high << change_candle.low << change_candle.volume << change_candle.differencelowhigh << change_candle.differenceopenclose << change_candle.maxprofitclose << change_candle.maxprofitlowhigh;
 
         candle copy = next_candle;
         candles.push(copy);
@@ -64,45 +87,6 @@ void process_csv(const char* in_file, const char* out)
     strcat(outpath, path.filename().string().c_str());
     csv.writeToFile(outpath, true);
 }
-
-
-//void process_csv(const char* in_file, const char* out)
-//{
-//    CSVWriter csv;
-//    io::CSVReader<8, io::trim_chars<' '>, io::no_quote_escape<','>> in(in_file);
-//
-//    in.read_header(io::ignore_missing_column | io::ignore_extra_column, "event_time", "kline_start_time", "kline_close_time", "open", "close", "high", "low", "volume");
-//    csv.newRow() << "event_time" << "kline_start_time" << "kline_close_time" << "open" << "close" << "high" << "low" << "volume";
-//
-//    std::stack<candle> candles;
-//    //std::stack<candle> processed_candles;
-//
-//    candle first_candle;
-//    if (!in.read_row(first_candle.event_time, first_candle.kline_start_time, first_candle.kline_close_time, first_candle.open, first_candle.close, first_candle.high, first_candle.low, first_candle.volume)) return;
-//    candles.push(first_candle);
-//
-//    candle next_candle;
-//    while (in.read_row(next_candle.event_time, next_candle.kline_start_time, next_candle.kline_close_time, next_candle.open, next_candle.close, next_candle.high, next_candle.low, next_candle.volume))
-//    {
-//        candle previous = candles.top();
-//        candles.pop();
-//
-//        candle change_candle = previous.difference(next_candle);
-//        //processed_candles.push(change_candle);
-//
-//        csv.newRow() << change_candle.event_time << change_candle.kline_start_time << change_candle.kline_close_time << change_candle.open << change_candle.close << change_candle.high << change_candle.low << change_candle.volume;
-//
-//        candle copy = next_candle;
-//        candles.push(copy);
-//    }
-//
-//    // write to file https://github.com/al-eax/CSVWriter
-//    char outpath[100];
-//    strcpy(outpath, out);
-//    std::filesystem::path path{ in_file };
-//    strcat(outpath, path.filename().string().c_str());
-//    csv.writeToFile(outpath, true);
-//}
 
 /**
  * @param {number} argc Argument count
@@ -132,8 +116,6 @@ int main(int argc, const char** argv)
 
     for (const auto file : std::filesystem::directory_iterator(input_folder))
     {
-        // & reference to all vars above
-        // = copy all vars above
         g_thread_pool->push([=]()
         {
             g_log->info("THREAD", "Processing file: %s", file.path().string().c_str());
